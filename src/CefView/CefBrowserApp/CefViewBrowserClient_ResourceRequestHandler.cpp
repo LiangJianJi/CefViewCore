eb95d62701320e0a9aea29a1eb8a03897ff9c016
ljj <635245147@qq.com>
2025-04-08 16:58:53 +0800

    如果请求头的 origin 字段为 file:// ，则移除此字段

--- src/CefView/CefBrowserApp/CefViewBrowserClient_ResourceRequestHandler.cpp
+++ src/CefView/CefBrowserApp/CefViewBrowserClient_ResourceRequestHandler.cpp
@@ -31,8 +31,42 @@ CefViewBrowserClient::OnBeforeResourceLoad(CefRefPtr<CefBrowser> browser,
 #endif
 )
 {
-  auto url = request->GetURL().ToString();
-  // logD("require resource: %s", url.c_str());
+  // url
+  CefString currentUrl = request->GetURL();
+  auto url = currentUrl.ToString();
+  //std::string httpPrefix = "http";
+  // origin
+  std::string originHeader = "origin";
+  CefString currentOrigin = request->GetHeaderByName(originHeader);
+  auto origin = currentOrigin.ToString();
+  //logD("require origin - before: %s", origin.c_str());
+  if (currentOrigin == "file://" /*&&
+      url.size() >= httpPrefix.size() &&
+      url.compare(0, httpPrefix.size(), httpPrefix) == 0*/) {
+#if 0
+    request->SetHeaderByName(originHeader, "", true);
+#else
+    // 获取请求头
+    CefRequest::HeaderMap headers;
+    request->GetHeaderMap(headers);
+    //std::string tmpKeyString;
+    //std::string tmpValueString;
+    //for each (auto var in headers) {
+    //  tmpKeyString = var.first.ToString();
+    //  tmpValueString = var.second.ToString();
+    //  logD("require headers: %s = %s", tmpKeyString.c_str(), tmpValueString.c_str());
+    //}
+    // 查找并移除 origin 的请求头字段
+    auto it = headers.find(originHeader);
+    if (it != headers.end()) {
+      headers.erase(it);
+    }
+    // 设置修改后的请求头
+    request->SetHeaderMap(headers);
+#endif
+  }
+  //logD("require origin - end: %s", origin.c_str());
+  //logD("require resource: %s", url.c_str());
   return resource_manager_->OnBeforeResourceLoad(browser, frame, request, callback);
 }
 
